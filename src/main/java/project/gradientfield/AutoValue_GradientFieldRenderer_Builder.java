301ac64729881cc0ae5352d5e7f14c80533eea2c
Wolfger Vanderper <wolfgervdp@gmail.com>
2018-05-19 18:22:33 +0200

    added implementation of GradientFieldRenderen_Builder

---
+++ src/main/java/project/gradientfield/AutoValue_GradientFieldRenderer_Builder.java
@@ -0,0 +1,20 @@
+package project.gradientfield;
+
+final class AutoValue_GradientFieldRenderer_Builder extends GradientFieldRenderer.Builder {
+
+    AutoValue_GradientFieldRenderer_Builder() {
+    }
+
+    public boolean equals(Object o) {
+        if (o == this) {
+            return true;
+        } else {
+            return o instanceof GradientFieldRenderer.Builder;
+        }
+    }
+
+    public int hashCode() {
+        int h = 1;
+        return h;
+    }
+}
